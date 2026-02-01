ba1377d9f678933eccad27c8dd7ccb1e67f5a128
rudrakshi-Dadwal <dadwalrudrakshi@gmail.com>
2026-02-01 12:01:37 +0530

    adding the bubble target basic js

--- bubble.js
+++ bubble.js
@@ -1,4 +1,4 @@
-console.log("bubble.js connected");
+function makebubble(){
 
 
 var clutter="";
@@ -9,3 +9,48 @@ for (var i=1;i<=119;i++){
 document.getElementById("pbtm").innerHTML=clutter;
 
 
+}
+makebubble();
+
+var timer=60;
+function runtimer(){
+    var timerint=setInterval(function(){
+    if(timer>0){   
+     timer--;
+   document.getElementById("timerval").innerText=timer;
+
+  } 
+else{
+    clearInterval(timerint);
+    alert("Time up! Game Over");
+} },1000);
+}
+
+runtimer();
+
+
+function getnewhit(){
+
+   var rn = Math.floor(Math.random()*10);
+    document.getElementById("hitval").innerText=rn;
+
+}
+
+getnewhit();
+
+var score=0;
+
+function getscore(){
+score+=10;
+document.getElementById("scoreval").innerText=score;
+}
+
+
+//jo element pe click karenge vo event raise hoga . event lisytener na milne oar evennt parent ke oarent ke parent par listener fhundega
+document.getElementById("pbtm").addEventListener("click",function(dets){
+   
+console.log(Number(dets.target.textContent)); // vo bubble jispe click hua
+//text content pe sirf number aagya naaki the whole div and vo num ber string hoga toh convert it 
+
+
+});
